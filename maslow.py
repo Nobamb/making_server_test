6459b9f8785a0cf3f5845548dbf62446d1d02283
nobamb <jsh12345644@gmail.com>
2026-01-18 17:42:19 +0900

    maslow 서버에서 basic_tag_complete 함수 테스트를 통해 동작 테스트

--- maslow.py
+++ maslow.py
@@ -2,6 +2,11 @@
 # http.server에서
 # basehttp, httpserver 가져오기
 from http.server import BaseHTTPRequestHandler, HTTPServer
+# basic_tag_complete 함수 가져오기
+from tag.basic_tag_complete import basic_tag_complete
+# tag들 가져오기
+from tag.open_tag import open_tag
+from tag.open_close_tag import open_close_tag
 
 
 # basehttp 클래스 오버라이드
@@ -19,6 +24,26 @@ class MaslowServer(BaseHTTPRequestHandler):
         
         # 헤더 전송 종료
         self.end_headers()
+        
+        
+        # h1 태그
+        # open_close_tag
+        # 내용 : 매슬로우의 욕구이론
+        h1 = open_close_tag("h1",childrens=["매슬로우의 욕구 이론"])
+        
+        # p 태그
+        # open_close_tag
+        # 내용 : 매슬로우의 욕구이론 테스트
+        p = open_close_tag("p",childrens=["매슬로우의 욕구이론 테스트"])
+        
+        
+        # basic_tag_complete 함수 사용해보기
+        # 제목 매슬로우
+        result = basic_tag_complete(title_name="매슬로우", add_body_tags=[h1, p])
+        
+        # 서버에 출력
+        # encode utf-8
+        self.wfile.write(result.encode("utf-8"))
         
         # 연결 테스트
         print("서버 접속 성공")
