e8fd7575c1c70afafa946259b663864a11dc2aeb
nobamb <jsh12345644@gmail.com>
2026-01-19 21:45:08 +0900

    태그명 관련 데이터 저장

---
+++ data/TagName.py
@@ -0,0 +1,12 @@
+# 태그 이름과 관련된 데이터 모음
+class TagName:
+    
+    br = "br"
+    h1 = "h1"
+    h2 = "h2"
+    p = "p"
+    script = "script"
+    link = "link"
+    div = "div"
+    ul = "ul"
+    li = "li"
