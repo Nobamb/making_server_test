15ed5dcfbd9d20a4d327aa929476eddf262ba156
nobamb <jsh12345644@gmail.com>
2026-01-22 11:20:54 +0900

    create_element.py 생성 및 주석 작성

---
+++ tag/react_style/create_element.py
@@ -0,0 +1,7 @@
+# 리액트 컴포넌트의 느낌으로 만들어보기
+# 태그명, children(튜플(변경 불가한 배열) 형식), props(딕셔너리 형식)을 받음
+# props를 받으면 " 키=값"의 형식으로 받음
+# 만약에 key를 className을 받으면 
+# className => class로 변형
+# children을 join 문법을 통해 묶음
+# f 스트링을 통해 tag_name, children, props값들을 모두 대입
