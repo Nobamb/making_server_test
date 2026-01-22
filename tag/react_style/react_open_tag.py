85e31e5e437dc99ce4ecebd1fa4a9eb21a91028f
nobamb <jsh12345644@gmail.com>
2026-01-22 13:09:01 +0900

    react_open_tag.py 작성 완료

--- tag/react_style/react_open_tag.py
+++ tag/react_style/react_open_tag.py
@@ -7,3 +7,25 @@
 # => **kwargs의 형태로 만들기(**props)
 # props를 items()로 분해하여 key, value로 나누기
 # f-string을 통해 문자열들 한번에 정리한 후 반환
+
+
+# react_open_tag 지정
+def react_open_tag(tag_name, **props):
+    # props를 받을 배열값 초기화
+    props_arr = []
+    # props의 key, value를 for문을 통해 순회
+    # items()를 사용하여 value까지 꺼내오기 편하게 지정
+    for key, value in props.items():
+        # props_arr에 추가할 문자열 지정
+        # key = value의 형태
+        key_value = f' {key}="{value}"'
+        # props_arr에 추가
+        props_arr.append(key_value)
+    # 추가가 완료되면 join으로 묶기
+    props_value = "".join(props_arr)
+    
+    # tag_name, props_value를 통해 open_tag 작성
+    # result의 형태로 지정
+    result = f"<{tag_name}{props_value}>"
+    # result 반환
+    return result
