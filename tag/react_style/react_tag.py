d00d235e833e8572193a4279c50244c36a77586e
nobamb <jsh12345644@gmail.com>
2026-01-22 14:15:07 +0900

    주석 일부 삭제(children 사용은 안하기에)

--- tag/react_style/react_tag.py
+++ tag/react_style/react_tag.py
@@ -1,14 +1,11 @@
 # 리액트 스타일대로 커스텀 태그를 만들어보기
+# (한번에 열고 닫는 태그)
 # 태그명, children, props를 받음
-# children은 튜플의 형태로 받도록
-# 단일 데이터를 children으로 받아서 내부에서 합치기
-# => *args의 형태로 만들기(*children)
 # props는 객체의 형태로 받도록
 # key:value느낌의 데이터를 key = value의 형식으로 단순하게 표현
 # props의 데이터들을 묶어서 딕셔너리 형식으로 만들기
 # => **kwargs의 형태로 만들기(**props)
 # props를 items()로 분해하여 key, value로 나누기
-# children은 join을 통해 묶기
 # f-string을 통해 문자열들 한번에 정리한 후 반환
 
 def react_tag(tag_name,**props):
@@ -39,9 +36,3 @@ def react_tag(tag_name,**props):
     
     # result 반환
     return result
-
-
-
-# 테스트
-hello = react_tag("div",className ="hello")
-print(hello)
