c5d999dcbacc7841d5a7ee29d6c124a863d70367
nobamb <jsh12345644@gmail.com>
2026-01-22 16:32:19 +0900

    테스트 주석처리

--- tag/react_style/react_tag.py
+++ tag/react_style/react_tag.py
@@ -52,6 +52,6 @@ def react_tag(tag_name,**props):
     return result
 
 
-# 테스트 
-div = react_tag("div",className = "hello")
-print(div)
+# # 테스트 
+# div = react_tag("div",className = "hello")
+# print(div)
