0394a8cb8ca752a3367370351869b582a5fca29d
nobamb <jsh12345644@gmail.com>
2026-01-23 12:00:02 +0900

    maslow_data.py 생성 및 기본 주석 작성

---
+++ data/maslow_data.py
@@ -0,0 +1,9 @@
+# maslow 서버에 대한 데이터들 모음
+# 배열 형식으로 전체 데이터를 모은 후
+# 객체 형식으로 각 데이터의 전체 값들을 저장
+# tag_type(태그타입)tag_name(태그명), text(내용), prop_name(요소 속성명), prop_value(요소 값)
+
+
+maslow_data =[
+            
+]
