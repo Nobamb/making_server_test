2667a8b2a7838a2d0534cd9f7b2b9bef78516c15
nobamb <jsh12345644@gmail.com>
2026-01-23 11:48:07 +0900

    react_basic_tag_complete 동작 확인 테스트

---
+++ test_react_basic_tag_complete.py
@@ -0,0 +1,10 @@
+# react_basic_tag_complete가 되는지 테스트
+from tag.react_style.react_basic_tag_complete import react_basic_tag_complete
+
+# 서버 가져오기
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+# 서버 구조 작성
+# TestBasicServer
+class TestBasicServer(BaseHTTPRequestHandler):
+    pass
