6eb7e8ce06cf1ace5dae0f6b974d4a217b2d6c3a
nobamb <jsh12345644@gmail.com>
2026-01-23 11:33:57 +0900

    구조 변경 및 테스트(for문으로는 head와 body를 보낼 방법이 생각이 안나서)

--- tag/react_style/react_basic_tag_complete.py
+++ tag/react_style/react_basic_tag_complete.py
@@ -3,9 +3,6 @@ from react_tag import react_tag
 from react_open_tag import react_open_tag
 from react_open_close_tag import react_open_close_tag
 
-# data 가져오기
-from data.maslow_basic_data import maslow_basic_data
-
 # 기본 형식의 html 구조 생성
 # <!DOCTYPE html>
 # <html lang="en">
@@ -15,34 +12,70 @@ from data.maslow_basic_data import maslow_basic_data
 #     <title>Document</title>
 # </head>
 # <body>
-    
+
 # </body>
 # </html>
 
+
 # 함수명 react_basic_tag_complete
-# head에 들어갈 값
-# body에 들어갈 값 지정
-# title 지정
-def react_basic_tag_complete(title_value, head, body):
-    # maslow_basic_data의 값들 가져와서 for문 돌려보기
-    for data in maslow_basic_data:
-        # 어떤 타입의 태그를 사용할 지 결정
-        # 초기값 none
-        how_to_tag = None
-        # 딕셔너리 형식의 데이터를 분해(key,value)
-        for key, value in data.items():
-            # 만약에 key가 tag_type일 때
-            if key == "tag_type":
-                # value가 tag면
-                if value == "tag":
-                    # react_tag 사용
-                    pass
-                # value가 open_tag면
-                if value == "open_tag":
-                    # react_open_tag 사용
-                    pass
-                # value가 open_close_tag면
-                if value == "open_close_tag":
-                    # react_open_tag 사용
-                    pass
-        
+# head에 들어갈 값(기본값 "")
+# body에 들어갈 값(기본값 "")
+# title 지정(기본값 document)
+def react_basic_tag_complete(title_value="document", head_value="", body_value=""):
+    # 각각의 데이터들을 지정
+    # doctype
+    # react_open_tag
+    # html 타입
+    doctype = react_open_tag("!DOCTYPE","html")
+    # <meta charset="UTF-8">
+    # react_open_tag사용
+    # 태그명 meta
+    # 타입명 charset
+    # 타입값 UTF-8
+    meta_charset = react_open_tag("meta",charset="UTF-8")
+    # <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    # react_open_tag사용
+    # 태그명 meta
+    # 타입명 name content
+    # 타입값 viewport width=device-width, initial-scale=1.0
+    meta_name_content = react_open_tag("meta",name = "viewport",content ="width=device-width, initial-scale=1.0")
+    
+    # title태그
+    # react_open_close_tag
+    # 태그명 title
+    # 값은 title_value
+    title = react_open_close_tag("title",title_value)
+    
+    # head 태그
+    # react_open_close_tag
+    # 태그명 head
+    # 값은 meta_charset, meta_name_content, title, head_value
+    head = react_open_close_tag("head",meta_charset, meta_name_content, title,head_value)
+    
+    
+    
+    # body 태그 
+    # react_open_close_tag
+    # 태그명 body
+    # 값은 body_value
+    body = react_open_close_tag("body",body_value)
+    
+    # html 태그
+    # react_open_close_tag
+    # 태그명 html
+    # 속성 lang="ko"
+    # 값은 head, body
+    html = react_open_close_tag("html",head, body)
+    
+    # 태그들의 합
+    result = doctype + html
+    
+    # 출력
+    return result
+
+
+
+# 테스트 
+result = react_basic_tag_complete()
+# 출력
+print(result)
