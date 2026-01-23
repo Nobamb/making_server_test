f3e6de3c3bc26596db9f0cf2433ccc7221d66b7f
nobamb <jsh12345644@gmail.com>
2026-01-23 09:26:54 +0900

    react_basic_tag_complete.py 생성 및 기본 동작 주석 작성

---
+++ tag/react_style/react_basic_tag_complete.py
@@ -0,0 +1,20 @@
+# react_style의 태그들 가져옴
+from react_tag import react_tag
+from react_open_tag import react_tag
+from react_open_close_tag import react_open_close_tag
+
+
+
+# 기본 형식의 html 구조 생성
+# <!DOCTYPE html>
+# <html lang="en">
+# <head>
+#     <meta charset="UTF-8">
+#     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+#     <title>Document</title>
+# </head>
+# <body>
+    
+# </body>
+# </html>
+
