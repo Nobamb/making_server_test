5526a5ae3352be55d7638ca7d765bfa995b6c7d4
nobamb <jsh12345644@gmail.com>
2026-01-23 17:13:35 +0900

    test_react_basic_tag_comlete 테스트 완료(maslow_data 가져옴)

--- test_react_basic_tag_complete.py
+++ test_react_basic_tag_complete.py
@@ -1,3 +1,9 @@
+# data_component_change 함수 가져오기
+from tag.react_style.data_component_change import data_component_change
+# maslow_data 가져오기
+from data.maslow_data import maslow_data
+
+
 # react_basic_tag_complete가 되는지 테스트
 from tag.react_style.react_basic_tag_complete import react_basic_tag_complete
 
@@ -16,8 +22,27 @@ class TestBasicServer(BaseHTTPRequestHandler):
         self.send_header("content-type","text/html; charset = utf-8")
         # header 전달 완료
         self.end_headers()
+                
+        
+        # maslow_data로 변환한 태그 데이터 배열 받기
+        maslow_data_to_html_arr = []
+        
+        
+        # maslow_data 가져오기
+        for data in maslow_data:
+            # data_component_change를 변환한 값 추가
+            # data를 파라미터값으로 줌
+            maslow_data_to_html = data_component_change(data)
+            # maslow_data_to_html_arr에 추가
+            maslow_data_to_html_arr.append(maslow_data_to_html)
+            
+        # maslow_data_to_html_arr의 배열 값들을 join
+        # \n으로 값들을 묶음
+        # body_result로 저장
+        body_result = "\n".join(maslow_data_to_html_arr)
+        
         # react_basic_tag_complete 실행값 저장
-        result = react_basic_tag_complete()
+        result = react_basic_tag_complete(body_value=body_result)
         # 값을 서버에 띄우도록 내용 전달
         self.wfile.write(result.encode("utf-8"))
         
