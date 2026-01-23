b4fceb1a7f9d0eaa6d8ad72187db4f436142781c
nobamb <jsh12345644@gmail.com>
2026-01-23 15:45:00 +0900

    data_component_change 생성(기존 데이터를 정제하여 react_style의 태그 함수들을 사용하기 위함) 및 데이터를 가져옴

---
+++ tag/react_style/data_component_change.py
@@ -0,0 +1,15 @@
+# 시스템(최상위 경로 추가 목적)
+import sys
+# 경로 가져오기
+from pathlib import Path
+
+# 최상위 경로 가져오기
+# 절대 경로 지정 후(resolve())
+# 현재 파일의 부모 : react_style => tag => making_server_test 순(3번 접근)
+top_path = Path(__file__).resolve().parent.parent.parent
+
+# 시스템에 top_path 추가
+sys.path.append(str(top_path))
+
+# data/maslow_data 데이터를 가져옴
+from data.maslow_data import maslow_data
