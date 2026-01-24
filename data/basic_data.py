5fe6b9887f361db73f096831d20cd252d04f4e95
nobamb <jsh12345644@gmail.com>
2026-01-24 19:36:26 +0900

    basic_data 주석 작성(주석 기반으로 데이터 작성해볼것)

---
+++ data/basic_data.py
@@ -0,0 +1,24 @@
+# 각 요소는 딕셔너리 형태,
+# 요소를 배열 단위로 받음
+# tag_name, tag_type, children(배열), props(딕셔너리)
+#     <!DOCTYPE html>
+    # <html lang="ko">
+    # <head>
+    #     <meta charset="UTF-8">
+    #     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    #     <title>Document</title>
+    # </head>
+    # <body>
+
+    # </body>
+    # </html>
+
+basic_data = [
+    
+    {
+        
+        
+    }
+    
+    
+]
