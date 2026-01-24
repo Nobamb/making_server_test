415296067398e6fde42e141ebfd3da9082494390
nobamb <jsh12345644@gmail.com>
2026-01-24 19:13:04 +0900

    ReactTag를 사용하여 메소드를 사용하면서 태그 값을 전달하도록 함

--- tag/react_style_class/data_component_change.py
+++ tag/react_style_class/data_component_change.py
@@ -1,3 +1,18 @@
+# 경로 변경
+from pathlib import Path
+import sys
+
+# 현재 파일을 절대경로로 지정
+# 상위 폴더를 지정(react_style_class)
+get_path = Path(__file__).resolve().parent
+
+# 시스템에 추가
+sys.path.append(get_path)
+
+# ReactTag import
+from react_style_class.ReactTag import ReactTag
+
+
 # data를 파라미터로 받음
 # 데이터를 받게 되면 ReactTag클래스로 넘기기
 def data_component_change(data):
@@ -36,3 +51,27 @@ def data_component_change(data):
         
     # children_arr의 값들을 모두 묶음
     children_value = "".join(children_arr)
+    
+    
+    
+    # ReactTag을 사용하여
+    # tag에 담음
+    # children_value는 *args로
+    # props는 **kwargs로
+    tag = ReactTag(tag_name,*children_value,**props)
+    
+    
+    
+    # tag_type에 따라 태그 메소드 사용 달라짐
+    # tag라면 get_tag사용하여 result 담음
+    if tag_type == "tag":
+        result = tag.get_tag()
+    # open_tag라면 get_open_tag사용하여 result 담음
+    if tag_type == "open_tag":
+        result = tag.get_open_tag()
+    # open_close_tag라면 get_open_close_tag사용하여 result 담음
+    if tag_type == "open_close_tag":
+        result = tag.get_open_close_tag()
+        
+    # result를 반환
+    return result
